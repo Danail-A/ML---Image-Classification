bd7dc3cd1a275347ca03e36d14cd0ec4f864f5d8
Danail-A <153531473+Danail-A@users.noreply.github.com>
2025-02-19 14:56:52 +0300

    Add files via upload

---
+++ translate_CNNproject.py
@@ -0,0 +1,5 @@
+translate = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat", "mucca": "cow", "pecora": "sheep", "scoiattolo": "squirrel", "dog": "cane", "cavallo": "horse", "elephant" : "elefante", "butterfly": "farfalla", "chicken": "gallina", "cat": "gatto", "cow": "mucca", "spider": "ragno", "squirrel": "scoiattolo"}
+
+name_num_dict = {"cane": 0, "cavallo": 1, "elefante": 2, "farfalla": 3, "gallina": 4, "gatto": 5, "mucca": 6, "pecora": 7, "scoiattolo": 8, "dog": 0, "horse": 1, "elephant": 2, "butterfly": 3, "chicken": 4, "cat": 5, "cow": 6, "spider": 9, "squirrel": 8}
+
+directory = r"kagglehub\datasets\alessiocorrado99\animals10\versions\2\raw-img"
